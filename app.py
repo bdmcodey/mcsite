10a421a7a765e3d73f5ad02aef57c37a250870e4
Matthew Codey <matthew.codey@gmail.com>
2023-10-10 17:20:52 -0700

    Fix app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '<img style="position: absolute; top: 50%; left: 50%; margin-top: -349px; margin-left: -392px;" src="static/img.jpg">'
+    return '<img style="position: absolute; top: 50%; left: 50%; \
+        margin-top: -349px; margin-left: -453px;" src="static/img.jpg">'
+
 
 if __name__ == '__main__':
     app.run()
-	
