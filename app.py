cfce406b6bb0e50e1faed7fa8b116332cd7a7d00
Matthew Codey <matthew.codey@gmail.com>
2026-07-24 12:43:00 -0700

    Redesign: professional multi-page site + CV

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', isIndex=True)
+    return render_template('index.html')
+
+
+@app.route('/projects/')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
+
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
-    app.run()
-
-
-@app.route('/home/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+    app.run(debug=True)
